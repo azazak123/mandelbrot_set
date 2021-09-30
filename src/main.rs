b1676cef8f39003ae37bdc743b2e0bab10ea93a8
azazak123 <azazaka2002@gmail.com>
2021-10-01 00:37:54 +0300

    add window mode and zoom

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,79 @@ use std::{
     sync::{Arc, Mutex},
 };
 
+use piston_window::*;
 use plotters::prelude::*;
+use plotters_piston::{draw_piston_window, PistonBackend};
 
-const ZOOM: f32 = 1.0/2.0;
-const RESOLUTION_X: usize = 4;
 const RESOLUTION_Y: usize = 1000;
+const RESOLUTION_X: usize = RESOLUTION_Y / THREAD_NUMBER;
 const WIDTH: u32 = 1000;
 const HEIGHT: u32 = 1000;
 const N: u32 = 3000;
 const THREAD_NUMBER: usize = 250;
+const FPS: u64 = 10;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let root = BitMapBackend::new("plotters-doc-data/0.png", (WIDTH, HEIGHT)).into_drawing_area();
-    root.fill(&WHITE)?;
-    let mut chart = ChartBuilder::on(&root)
-        .build_cartesian_2d((-1f32 / ZOOM)..(1f32 / ZOOM), (-1f32 / ZOOM)..(1f32 / ZOOM))?;
+fn main() {
+    let mut window: PistonWindow =
+        WindowSettings::new("Real Time CPU Usage", [WIDTH - 200, HEIGHT - 200])
+            .exit_on_esc(true)
+            .resizable(false)
+            .build()
+            .unwrap();
+    window.set_max_fps(FPS);
 
+    let mut ZOOM: f32 = 1.0;
+    let mut events = Events::new(EventSettings::new().lazy(true));
+    let mut generate = true;
+    loop {
+        if let Some(e) = events.next(&mut window) {
+            if let Some(Button::Keyboard(key)) = e.press_args() {
+                match key {
+                    Key::C => {
+                        generate = true;
+                    }
+                    Key::A => {
+                        return;
+                    }
+                    Key::Space => {
+                        println!("click");
+                    }
+                    Key::Plus => {
+                        ZOOM += 1.0 / 10.0;
+                        generate = true;
+                    }
+                    Key::Minus => {
+                        ZOOM -= 1.0 / 10.0;
+                        if ZOOM == 0.0 {
+                            ZOOM = 1.0 / 10.0
+                        }
+                        generate = true;
+                    }
+                    _ => {}
+                }
+            }
+        }
+        if !generate {
+            continue;
+        }
+        if let Some(_) = draw_piston_window(&mut window, |b| {
+            println!("generate");
+            let root = b.into_drawing_area();
+            root.fill(&WHITE)?;
+            let mut chart = ChartBuilder::on(&root)
+                .build_cartesian_2d((-1f32 / ZOOM)..(1f32 / ZOOM), (-1f32 / ZOOM)..(1f32 / ZOOM))?;
+
+            let set = mandelbrot_generate_threading(ZOOM);
+            let coords = set.into_iter();
+
+            chart.draw_series(coords.map(|coord| Pixel::new(coord, &BLACK)))?;
+            generate = false;
+            Ok(())
+        }) {}
+    }
+}
+
+fn mandelbrot_generate_threading(ZOOM: f32) -> Vec<(f32, f32)> {
     let mut handles = Vec::with_capacity(THREAD_NUMBER);
     let set_vectors = Arc::new(Mutex::new(vec![
         Vec::<(f32, f32)>::with_capacity(
@@ -47,34 +104,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     for i in &mut *set_vectors.lock().unwrap() {
         set.append(i);
     }
-    let coords = set.into_iter();
-
-    chart.draw_series(coords.map(|coord| Pixel::new(coord, &BLACK)))?;
-    Ok(())
+    set
 }
 
 fn mandelbrot_generate(x_range: Range<f32>, y_range: Range<f32>) -> Vec<(f32, f32)> {
     let dx = x_range.end - x_range.start;
     let dy = y_range.end - y_range.start;
-    let mut arr = vec![(0f32, 0f32); RESOLUTION_X * RESOLUTION_Y];
+    let mut result: Vec<(f32, f32)> = Vec::with_capacity(RESOLUTION_X * RESOLUTION_Y);
     let mut y = y_range.start;
     for i in 0..RESOLUTION_Y {
         let mut x = x_range.start;
         for j in 0..RESOLUTION_X {
-            arr[i * RESOLUTION_X + j] = (x, y);
-            x += dx / RESOLUTION_X as f32;
-        }
-        y += dy / RESOLUTION_Y as f32;
-    }
-
-    let mut result: Vec<(f32, f32)> = Vec::with_capacity(RESOLUTION_X * RESOLUTION_Y);
-    for i in 0..RESOLUTION_Y {
-        for j in 0..RESOLUTION_X {
             let mut x_current = 0f32;
             let mut y_current = 0f32;
-            let (p, q) = arr[i * RESOLUTION_X + j];
+            let p = x;
+            let q = y;
             let mut n = 0;
-            while n < N && (x_current * x_current - y_current * y_current) < 10f32.powf(2.0) {
+            while n < N && (x_current * x_current - y_current * y_current) < 2.0 {
                 let x_prev = x_current;
                 let y_prev = y_current;
                 x_current = x_prev * x_prev - y_prev * y_prev + p;
@@ -84,8 +130,9 @@ fn mandelbrot_generate(x_range: Range<f32>, y_range: Range<f32>) -> Vec<(f32, f3
             if n == N {
                 result.push((p, q));
             }
-            // println!("{}", i as f32 / RESOLUTION as f32);
+            x += dx / RESOLUTION_X as f32;
         }
+        y += dy / RESOLUTION_Y as f32;
     }
     result
 }
